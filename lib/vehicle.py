f36760a5a24aceb53abc410e7660c52125b067ba
Fiona Mellink <fimellink@gmail.com>
2024-04-08 10:16:47 +1000

    Complete lab

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,17 @@
 class Vehicle:
-    pass
+    #this is the parent or superclass
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_size = wheel_size
+        self.wheel_number = wheel_number
+
+    def go(self):
+        return "vrrrrrrrooom!"
+
+    def fill_up_tank(self):
+        return "filling up!"
+
+
+class Car(Vehicle):
+    #this is the subclass or child
+    def go(self):
+        return "VRRROOOOOOOOOOOOOOOOOOOOOOOM!!!!!"
